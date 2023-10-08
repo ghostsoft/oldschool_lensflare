41246c5848742af85ae38f128c64abccba89b9b7
ghostsoft <63552156+ghostsoft@users.noreply.github.com>
2023-10-08 15:40:00 +0200

    Added raycast
    Added raycast to check if sun is visible to cull the lens-flare ghosts

--- flare_ghost.gd
+++ flare_ghost.gd
@@ -1,5 +1,4 @@
 ## Helper class to allow editing the lens-flare ghosts in the editor
-
 class_name FlareGhost extends Resource
 
 @export var color : Color = Color(1,1,1,0.25)
--- lens_flare.gd
+++ lens_flare.gd
@@ -8,26 +8,26 @@ extends Node3D
 @export var lensflare_sprite : Texture2D
 # Default value 2.0
 # A value of 1.0 stretches from sun to screen center and 2.0 stretches twice as far
-@export var length = 2.0
-@export var size = 1.0
-# workaround for lack of editor buttons
+@export var length : float = 2.0
+@export var size : float = 1.0
+# Workaround for lack of editor buttons
 # clicking this will randomize the ghosts to a pleasant range of values
 @export var randomize_ghosts : bool = false :
 	set(value): _randomize(value)
 
 @export var ghosts : Array[FlareGhost]
 
-var camera
+var camera : Camera3D
 var ghost_instances : Array[Node3D]
 
-func _ready():
+func _ready() -> void:
 	if Engine.is_editor_hint():
 		return
 		
 	camera = get_viewport().get_camera_3d()
 	
 	for ghost in ghosts:
-		var ghost_instance = Sprite3D.new()
+		var ghost_instance : Node3D = Sprite3D.new()
 		ghost_instance.billboard = true
 		ghost_instance.fixed_size = true
 		ghost_instance.pixel_size = (size * 0.005) * ghost.size
@@ -37,32 +37,45 @@ func _ready():
 		add_child(ghost_instance)
 		ghost_instances.append(ghost_instance)
 
-func _process(_delta):
+func _process(_delta) -> void:
+	
 	if Engine.is_editor_hint():
 		return
 	
-	var sun_screen = camera.unproject_position(self.global_position)
-	var screen_center = get_viewport().get_visible_rect().get_center()
-	var sun_to_center = screen_center - sun_screen
+	# Raycast to check if the sun (center) is visible on screen
+	var raycast_result : Dictionary = get_world_3d().direct_space_state.intersect_ray(PhysicsRayQueryParameters3D.create(camera.global_position, self.global_position))
 
-	var bounds = get_viewport().get_visible_rect().grow(150)
+	# If the center of the sun is hidden, then we don't show the ghosts
+	if raycast_result.is_empty():
+		for ghost in ghost_instances:
+			ghost.visible = true
+	else:
+		for ghost in ghost_instances:
+			ghost.visible = false
+		return
 	
-	# check if sun is within bounds or behind camera
+	var sun_screen : Vector2 = camera.unproject_position(self.global_position)
+	var screen_center : Vector2 = get_viewport().get_visible_rect().get_center()
+	var sun_to_center : Vector2 = screen_center - sun_screen
+
+	var bounds : Rect2 = get_viewport().get_visible_rect().grow(150)
+	
+	# Check if sun is within bounds or behind camera
 	if not bounds.has_point(sun_screen) || camera.is_position_behind(self.global_position):
 		for ghost in ghost_instances:
 			ghost.visible = false
 		return
 	
 	for i in range(ghost_instances.size()):
-		# get the size of equally spaced offset from 0 to length
-		var sprite_offset = remap(1,0,ghost_instances.size(),0,length)
-		# offset the sprite along the sun_to_center vector starting from the sun
+		# Get the size of equally spaced offset from 0 to length
+		var sprite_offset : float = remap(1,0,ghost_instances.size(),0,length)
+		# Offset the sprite along the sun_to_center vector starting from the sun
 		# plus one sprite_offset so we don't start *inside* the sun
 		ghost_instances[i].global_position = camera.project_position(sun_screen + (sprite_offset+(i*sprite_offset)) * sun_to_center, 1)
 		ghost_instances[i].visible = true
 
-# randomizes the ghosts to a pleasant value
-func _randomize(_value = false):
+# Randomizes the ghosts to a pleasant value
+func _randomize(_value = false) -> void:
 	for i in range(ghosts.size()):
 		if ghosts[i] == null:
 			print_debug("No ghost instantiated in index " + str(i))
